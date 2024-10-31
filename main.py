3fb944d2ef071533a5fecfe8437b45e5a9231e63
weslley343 <weslleydavid343@gmail.com>
2024-10-31 15:04:33 -0300

    cors

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from router.pessoa import pessoa_router  # Importa o router do arquivo router/pessoa.py
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas as origens
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
 
 # Inclui as rotas
 app.include_router(pessoa_router, prefix="/api", tags=["pessoas"])
